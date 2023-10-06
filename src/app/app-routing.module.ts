d94bc988a7a59f9d6d002f805bd520ce2ffa8cd9
João Rocha <joaomarcus12769@gmail.com>
2023-10-05 22:36:17 -0300

    feat: add multi-buttons-loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MultiButtonsLoadingComponent } from './multi-buttons-loading/multi-buttons-loading.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'multi-buttons-loading', component: MultiButtonsLoadingComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/multi-buttons-loading/multi-buttons-loading.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-multi-buttons-loading',
+  templateUrl: './multi-buttons-loading.component.html',
+  styleUrls: ['./multi-buttons-loading.component.scss']
+})
+export class MultiButtonsLoadingComponent {
+
+}
