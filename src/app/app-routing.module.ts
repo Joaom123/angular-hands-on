a3726b49d98bccc97eaef49eee024b6e77d4ed70
João Rocha <joaomarcus12769@gmail.com>
2023-10-06 19:36:38 -0300

    feat: add ResponsiveFormsComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MultiButtonsLoadingComponent } from './multi-buttons-loading/multi-buttons-loading.component';
+import { ResponsiveFormsComponent } from './responsive-forms/responsive-forms.component';
 
-const routes: Routes = [{ path: 'multi-buttons-loading', component: MultiButtonsLoadingComponent },];
+const routes: Routes = [
+  { path: 'multi-buttons-loading', component: MultiButtonsLoadingComponent },
+  {
+    path: 'responsive-forms',
+    component: ResponsiveFormsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MultiButtonsLoadingComponent } from './multi-buttons-loading/multi-buttons-loading.component';
+import { ResponsiveFormsComponent } from './responsive-forms/responsive-forms.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MultiButtonsLoadingComponent
+    MultiButtonsLoadingComponent,
+    ResponsiveFormsComponent
   ],
   imports: [
     BrowserModule,
