869b37a23f0ce482da19bb1f3b26f0835696d082
João Rocha <joaomarcus12769@gmail.com>
2023-10-05 23:51:02 -0300

    feat; add loading dictionary

--- src/app/multi-buttons-loading/loading.service.ts
+++ src/app/multi-buttons-loading/loading.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoadingService {
+  private loadingStates: { [buttonId: string]: BehaviorSubject<boolean> } = {};
 
-  constructor() { }
+  setLoadingState(buttonId: string, state: boolean): void {
+    if (!this.loadingStates[buttonId]) {
+      this.loadingStates[buttonId] = new BehaviorSubject<boolean>(false);
+    }
+    this.loadingStates[buttonId].next(state);
+  }
+
+  getLoadingState(buttonId: string): BehaviorSubject<boolean> {
+    if (!this.loadingStates[buttonId]) {
+      this.loadingStates[buttonId] = new BehaviorSubject<boolean>(false);
+    }
+    return this.loadingStates[buttonId];
+  }
 }
--- src/app/multi-buttons-loading/multi-buttons-loading.component.ts
+++ src/app/multi-buttons-loading/multi-buttons-loading.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LoadingService } from './loading.service';
+import { Observable, delay, of, tap } from 'rxjs';
 
 @Component({
   selector: 'app-multi-buttons-loading',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./multi-buttons-loading.component.scss']
 })
 export class MultiButtonsLoadingComponent {
+
+  constructor(private loadingService: LoadingService) {}
+
+  fetchData(buttonId: string): void {
+    of([])
+    .pipe(
+      tap(() => this.loadingService.setLoadingState(buttonId, true)),
+      delay(5000)
+    )
+    .subscribe(
+      (data) => {
+        this.loadingService.setLoadingState(buttonId, false);
+      }
+    );
+  }
+
+  getLoadingState(buttonId: string): Observable<boolean> {
+    return this.loadingService.getLoadingState(buttonId);
+  }
 
 }
